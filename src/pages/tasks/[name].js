1db3e1af69c85f9f5ba128334d8589352e6a7da4
MagirusDeutz <masiomc@gmail.com>
2022-10-18 17:27:34 -0300

    Adding filter responsiveness and question details

--- src/pages/tasks/[name].js
+++ src/pages/tasks/[name].js
@@ -1,4 +1,4 @@
-import { Box, Checkbox, FormControlLabel, Popover, Typography } from "@mui/material";
+import { Box, Popover } from "@mui/material";
 import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -7,10 +7,12 @@ import { DashboardLayout } from "../../components/dashboard-layout";
 import questions from "../../data/questions.json";
 import { Exercices } from "../../components/quests/exercices"
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Filter } from "../../components/quests/filter";
 
 const Tasks = () => {
   const router = useRouter()
   const [isTopicsOpen, setTopicsOpen] = useState(null)
+  const [isFilterOpen, setFilterOpen] = useState(null)
 
   const handleClick = (event) => {
     setTopicsOpen(event.currentTarget);
@@ -20,8 +22,19 @@ const Tasks = () => {
     setTopicsOpen(null);
   };
 
+  const clickFilter = (event) => {
+    setFilterOpen(event.currentTarget);
+  };
+
+  const filterClose = () => {
+    setFilterOpen(null);
+  };
+
+
   const open = Boolean(isTopicsOpen);
-  const id = open ? 'simple-popover' : undefined;
+  const unClose = Boolean(isFilterOpen);
+  const topics = open ? 'simple-popover' : undefined;
+  const filter = unClose ? 'simple-popover' : undefined;
 
 
   return(
@@ -38,16 +51,15 @@ const Tasks = () => {
         }}
       >
         <Box className="bg-[#1F2937] w-full p-4 px-6">
-          <div className="text-[30px] lg:text-[2rem] font-semibold text-white max-w-[1150px] mx-auto">
-            <button aria-describedby={id} 
+          <div className="text-[30px] lg:text-[2rem] font-semibold text-white max-w-[1150px] mx-auto flex justify-between">
+            <button aria-describedby={topics} 
             onClick={handleClick} 
             className="hover:text-green-500">
               {router.query.name}
               <ExpandMoreIcon fontSize="20" />
-            </button>
-            
+            </button>           
             <Popover
-              id={id}
+              id={topics}
               open={open}
               anchorEl={isTopicsOpen}
               onClose={handleClose}
@@ -61,6 +73,32 @@ const Tasks = () => {
               }}
             >
               <Exercices />
+            </Popover>
+
+            <button
+            className="text-[15px] lg:hidden text-[#21a87b] border border-[#3FC79A] px-2 rounded hover:bg-[#21a87b] hover:text-[#1F2937]" 
+            aria-describedby={filter} 
+            onClick={clickFilter} >
+              Filtro
+            </button>
+            <Popover
+              id={filter}
+              open={unClose}
+              anchorEl={isFilterOpen}
+              onClose={filterClose}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+            >
+              <div className="mt-1 mb-1">
+                <Filter />
+              </div>
+              
             </Popover>
             
           </div>
@@ -76,8 +114,18 @@ const Tasks = () => {
               <Box className="group">
                 <button className="p-4 border mb-3 border-gray-500 group-hover:border-green-500 w-full rounded">
                   <Box className="font-semibold justify-between flex">
-                    <Box className="group-hover:text-green-500 p-2">
-                    {question.task}
+                    <Box className="p-2">
+                      <div className="group-hover:text-green-500 flex">
+                        {question.task}
+                      </div>
+                      <div className="text-[13px] flex">
+                        <div className="text-green-500">
+                        {question.dificuldade}
+                        </div>
+                        <div>
+                        , Taxa de Sucesso: {question.taxaSucesso}
+                        </div>
+                      </div>
                     </Box>
                     <Box className="mb-4 p-2 border border-green-500 group-hover:bg-green-500 text-green-500 group-hover:text-[#1F2937] rounded">
                       Resolver Desafio
@@ -92,53 +140,8 @@ const Tasks = () => {
           </Link>
           ))}
           </div>
-          <Box className="p-2 h-full border border-gray-500 rounded flex flex-col">
-            <div className="flex flex-col border-b border-gray-500 w-full px-4">
-              <Typography
-                color="textSecondary"
-                gutterBottom
-                variant="h6"
-              >
-                Condição
-              </Typography>
-              <FormControlLabel
-                control={<Checkbox />}
-                label="Resolvido"
-              />
-              <FormControlLabel
-                control={<Checkbox />}
-                label="Não Resolvido"
-              />           
-          </div>
-          <div className="flex flex-col pt-2 px-4">
-            <Typography
-              color="textSecondary"
-              gutterBottom
-              variant="h6"
-            >
-              Dificuldade
-            </Typography>
-            <FormControlLabel
-              control={<Checkbox />}
-              label="Iniciante"
-            />  
-            <FormControlLabel
-              control={<Checkbox />}
-              label="Fácil"
-            />  
-            <FormControlLabel
-              control={<Checkbox />}
-              label="Médio"
-            />   
-            <FormControlLabel
-              control={<Checkbox />}
-              label="Difícil"
-            />  
-            <FormControlLabel
-              control={<Checkbox />}
-              label="Expert"
-            />  
-          </div>
+          <Box className="h-full lg:flex flex-col hidden">
+            <Filter />
             
           </Box>
 
