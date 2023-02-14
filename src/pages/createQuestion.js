198348452bb799609023c99870b20b9148a95625
MasioCesar <masiomc@gmail.com>
2023-02-14 20:35:53 -0300

    restricting create questions page

--- src/pages/createQuestion.js
+++ src/pages/createQuestion.js
@@ -1,10 +1,21 @@
+import { Box } from '@mui/material';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
+import { useState } from 'react';
 import { DashboardLayout } from '../components/dashboard-layout';
+import { UserDetails } from '../requestsFirebase/allGetRequests';
+import { Loader } from '../requestsFirebase/loader';
 
 const DatasQuestion = dynamic(import('../components/createQuestion/datasQuestion'), { ssr: false })
 
 const CreateQuestion = () => {
+    const [coders, setCoders] = useState()
+    UserDetails(setCoders)
+
+    if (!coders || coders.teacher === false) {
+        return <> <Loader /> </>
+    }
+
     return(
     <>
         <Head>
