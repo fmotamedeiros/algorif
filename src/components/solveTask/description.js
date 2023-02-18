cf8b4aac89d09ca37e77661fbfa52b364c94a63c
MasioCesar <masiomc@gmail.com>
2023-02-17 23:18:22 -0300

    adding input and output example in question description

--- src/components/solveTask/description.js
+++ src/components/solveTask/description.js
@@ -1,8 +1,38 @@
-import { TextField } from "@mui/material"
+import { TextField, Grid } from "@mui/material"
 
-const Description = ({ description }) => {
+const Description = ({ descriptionData }) => {
     return (
-        <TextField fullWidth multiline value={description} disabled /> 
+
+        <>
+            <TextField
+                fullWidth
+                multiline
+                disabled
+                value={descriptionData.detailedDescription}
+            />
+
+            <Grid container spacing={2} mt={2}>
+                <Grid item xs={6}>
+                    <TextField
+                        fullWidth
+                        multiline
+                        disabled
+                        label="Valor da Entrada"
+                        value={descriptionData.test[0].input}
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField
+                        fullWidth
+                        multiline
+                        disabled
+                        label="Valor da Saída"
+                        value={descriptionData.test[0].output}
+                    />
+                </Grid>
+            </Grid>
+        </>
+
     )
 }
 
