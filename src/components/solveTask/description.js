5c4ccec3601fc89c9fd8d8cbe358373d948c30e0
MasioCesar <masiomc@gmail.com>
2023-04-25 03:14:43 -0300

    changing how the chatbot closes and opens

--- src/components/solveTask/description.js
+++ src/components/solveTask/description.js
@@ -1,76 +1,65 @@
 import { useState } from "react";
-import { TextField, Grid, Button, Popover, DialogTitle, DialogContent } from "@mui/material";
+import { TextField, Grid, Button } from "@mui/material";
+
+const ChatBox = ({ open }) => {
+    return (
+        <>
+            {open && (
+                <div>
+                    <iframe
+                        allow="microphone;"
+                        width="350"
+                        height="400"
+                        src="https://console.dialogflow.com/api-client/demo/embedded/4d792e49-e0aa-4a22-b515-77be57ce4804"
+                    />
+                </div>
+            )}
+        </>
+    );
+};
 
 const Description = ({ descriptionData }) => {
-    const [anchorEl, setAnchorEl] = useState(null);
+    const [open, setOpen] = useState(false);
+    const [buttonText, setButtonText] = useState("Iniciar conversa");
+    const [buttonColor, setButtonColor] = useState("primary");
 
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
+    const handleClick = () => {
+        if (!open) {
+            setOpen(true);
+            setButtonText("Encerrar conversa");
+            setButtonColor("error");
+        } else {
+            setOpen(false);
+            setButtonText("Iniciar conversa");
+            setButtonColor("primary");
+        }
     };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-
-    const open = Boolean(anchorEl);
 
     return (
         <>
-            <TextField
-                fullWidth
-                multiline
-                disabled
-                value={descriptionData.detailedDescription}
-            />
+            <TextField fullWidth multiline disabled value={descriptionData.detailedDescription} />
 
             <Grid container spacing={2} mt={2}>
                 <Grid item xs={6}>
-                    <TextField
-                        fullWidth
-                        multiline
-                        disabled
-                        label="Valor da Entrada"
-                        value={descriptionData.test[0].input}
-                    />
+                    <TextField fullWidth multiline disabled label="Valor da Entrada" value={descriptionData.test[0].input} />
                 </Grid>
                 <Grid item xs={6}>
-                    <TextField
-                        fullWidth
-                        multiline
-                        disabled
-                        label="Valor da Saída"
-                        value={descriptionData.test[0].output}
-                    />
+                    <TextField fullWidth multiline disabled label="Valor da Saída" value={descriptionData.test[0].output} />
                 </Grid>
             </Grid>
 
-            <div className="bottom-6 left-20 p-2 flex justify-start items-center lg:fixed">
-                <img src="/static/images/chatbot.png" alt="Cartoon Character" className="w-20 h-20 mr-2" />
-                <Button variant="contained" color="primary" onClick={handleClick}>
-                    Iniciar conversa
-                </Button>
+            <div className="bottom-6 p-2 lg:fixed lg:pt-24">
+                <div className="flex items-center">
+                    <img src="/static/images/chatbot.png" alt="Cartoon Character" className="w-20 h-20 mr-2" />
+                    <Button variant="contained" color={buttonColor} onClick={handleClick} >
+                        {buttonText}
+                    </Button>
+                </div>
+                <div className="pt-1">
+                    <ChatBox open={open} />
+                </div>
             </div>
 
-            <Popover
-                open={open}
-                anchorEl={anchorEl}
-                onClose={handleClose}
-                anchorOrigin={{
-                    vertical: "bottom",
-                    horizontal: "left",
-                }}
-                transformOrigin={{
-                    vertical: "top",
-                    horizontal: "left",
-                }}
-            >
-                <iframe
-                    allow="microphone;"
-                    width="350"
-                    height="430"
-                    src="https://console.dialogflow.com/api-client/demo/embedded/4d792e49-e0aa-4a22-b515-77be57ce4804"
-                />
-            </Popover>
         </>
     );
 };
