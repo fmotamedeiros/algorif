bb50e2f280572c79608d8edc84990d62a9685e56
Nícolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-08-21 13:16:52 -0300

    Campo de função removida no cadastro de questão

--- src/components/createQuestion/datasQuestion.jsx
+++ src/components/createQuestion/datasQuestion.jsx
@@ -1,7 +1,7 @@
 import {
-    Box,
-    Button,
-    Typography
+  Box,
+  Button,
+  Typography
 } from '@mui/material';
 import { SetContext } from '../../contexts/setFirebase';
 import { useContext, useEffect, useRef } from 'react';
@@ -24,207 +24,178 @@ const navTopics = getNavTopics()
 
 
 const DatasQuestion = () => {
-    const codeEditorRef = useRef(null);
+  const codeEditorRef = useRef(null);
 
-    useEffect(() => {
-        if (codeEditorRef.current) {
-            return
-        }
-        const codeEditor = CodeMirror.fromTextArea(document.getElementById('code-editor'), {
-            mode: "javascript",
-            theme: 'dracula',
-            lineNumbers: true,
-            autoCloseTags: true,
-            autoCloseBrackets: true
-        });
-        codeEditor.setSize("100%", 520)
+  const formik = useFormik({
+      initialValues: {
+          topic: '',
+          title: '',
+          description: '',
+          detailedDescription: '',
+          difficulty: '',
+          tests: [
+              { inputTest: '', outputTest: '' },
+          ],
+      },
+      validationSchema: Yup.object({
+          topic: Yup
+              .string()
+              .max(255)
+              .required('Tópico da questão é obrigatório'),
+          title: Yup
+              .string()
+              .max(255)
+              .required('Título é obrigatório'),
+          description: Yup
+              .string(),
+          detailedDescription: Yup
+              .string()
+              .required('Descrição detalhada é obrigatório'),
+          difficulty: Yup
+              .string()
+              .max(255)
+              .required('difficulty é obrigatório'),
+          tests: Yup.array().of(
+              Yup.object({
+                  inputTest: Yup
+                      .string()
+                      .max(255)
+                      .required('Teste de entrada é obrigatório'),
+                  outputTest: Yup
+                      .string()
+                      .required('Teste de saída é obrigatório'),
+              })
+          ),
+      }),
+      onSubmit: async () => {
+          await setContext.setCreateQuestion(formik.values, codeEditorRef.current.getValue())
+          alert('Questão Cadastrada com Sucesso')
+          Router.reload()
+      }
+  })
 
-        codeEditor.setValue(
-            `function main(/*variáveisDeEntradas*/) {\n    return\n} \n\nconsole.log(main())`)
+  const addTest = () => {
+      formik.setValues({
+          ...formik.values,
+          tests: [...formik.values.tests, { inputTest: '', outputTest: '' }]
+      });
+  };
 
-        codeEditorRef.current = codeEditor;
-    }, []);
+  const setContext = useContext(SetContext);
 
-    const outputResult = () => {
-        try {
-            eval(codeEditorRef.current.getValue()); // executar o código
-            output.innerHTML = consoleWritten.map(line => line + "<br>").join("\n");
-            consoleWritten = []
-        } catch (error) {
-            output.textContent = error.toString();
-        }
-    };
+  return (
+      <>
+          <div className='px-[5%]'>
+              <Box
+                  component="main"
+                  sx={{
+                      flexGrow: 1,
+                      py: 8
+                  }}
+              >
+                  <form onSubmit={formik.handleSubmit}
+                      autoComplete="off"
+                      noValidate
+                  >
+                      <Box>
+                          <Typography
+                            color="primary.light"
+                            variant="h4"
+                          >
+                            Criar nova questão
+                          </Typography>
+                          <Box className='grid grid-cols-2 gap-x-4'>
+                              <CustomTextField
+                                  formik={formik}
+                                  label="Selecione um tópico"
+                                  name="topic"
+                                  options={navTopics}
+                                  required
+                                  select
+                              />
+                              <CustomTextField
+                                  formik={formik}
+                                  label="Título da questão"
+                                  name="title"
+                                  required
+                              />
+                              <CustomTextField
+                                  formik={formik}
+                                  label="Selecione a dificuldade"
+                                  name="difficulty"
+                                  options={navDifficulties}
+                                  required
+                                  select
+                              />
+                              <CustomTextField
+                                  formik={formik}
+                                  label="Descrição da questão"
+                                  name="description"
+                              />
+                          </Box>
+                          <CustomTextField
+                              formik={formik}
+                              label="Descrição detalhada da questão"
+                              name="detailedDescription"
+                              rows={10}
+                              multiline
+                              required
+                          />
 
-    // redefinir console.log para adicionar saída ao elemento de saída
-    let consoleWritten = [];
-    const handleRun = () => {
-        console.log = (...args) => {
-            consoleWritten.push(...args);
-        };
-        outputResult();
-    };
+                          <Typography
+                            color="primary.light"
+                            variant="h5"
+                          >
+                            Dados da função
+                          </Typography>
 
-    const formik = useFormik({
-        initialValues: {
-            topic: '',
-            title: '',
-            description: '',
-            detailedDescription: '',
-            difficulty: '',
-            tests: [
-                { inputTest: '', outputTest: '' },
-            ],
-        },
-        validationSchema: Yup.object({
-            topic: Yup
-                .string()
-                .max(255)
-                .required(
-                    'Tópico da questão is required'),
-            title: Yup
-                .string()
-                .max(255)
-                .required(
-                    'Título is required'),
-            description: Yup
-                .string()
-                .required('Descricão is required'),
-            detailedDescription: Yup
-                .string()
-                .required('Descricão detalhada is required'),
-            difficulty: Yup
-                .string()
-                .max(255)
-                .required('difficulty is required'),
-            tests: Yup.array().of(
-                Yup.object({
-                    inputTest: Yup
-                        .string()
-                        .max(255)
-                        .required('Teste de entrada é obrigatório'),
-                    outputTest: Yup
-                        .string()
-                        .required('Teste de saída é obrigatório'),
-                })
-            ),
-        }),
-        onSubmit: async () => {
-            await setContext.setCreateQuestion(formik.values, codeEditorRef.current.getValue())
-            alert('Questão Cadastrada com Sucesso')
-            Router.reload()
-        }
-    })
+                          <CustomTextField
+                              formik={formik}
+                              label="Parâmetros da função"
+                          />
 
-    const addTest = () => {
-        formik.setValues({
-            ...formik.values,
-            tests: [...formik.values.tests, { inputTest: '', outputTest: '' }]
-        });
-    };
+                          {formik.values.tests && formik.values.tests.map((test, index) => (
+                              <Box className='grid grid-cols-2 gap-x-4' key={`${test}-${index}`}>
+                                  <CustomTextField
+                                      formik={formik}
+                                      label={`${index + 1}° entrada dos dados`}
+                                      name={`tests.${index}.inputTest`}
+                                      value={test.inputTest}
+                                  />
 
-    const setContext = useContext(SetContext);
-
-    return (
-        <>
-            <div className='px-[5%]'>
-                <Box
-                    component="main"
-                    sx={{
-                        flexGrow: 1,
-                        py: 8
-                    }}
-                >
-                    <form onSubmit={formik.handleSubmit}
-                        autoComplete="off"
-                        noValidate
-                    >
-                        <Box>
-                            <Typography
-                                color="textPrimary"
-                                variant="h4"
-                            >
-                                Criar uma nova questão
-                            </Typography>
-                            <Box className='grid grid-cols-2 gap-x-4'>
-                                <CustomTextField
-                                    formik={formik}
-                                    label="Selecione o Tópico"
-                                    name="topic"
-                                    select
-                                    options={navTopics}
-                                />
-                                <CustomTextField
-                                    formik={formik}
-                                    label="Título da Questão"
-                                    name="title"
-                                />
-                                <CustomTextField
-                                    formik={formik}
-                                    label="Selecione a dificuldade"
-                                    name="difficulty"
-                                    select
-                                    options={navDifficulties}
-                                />
-                                <CustomTextField
-                                    formik={formik}
-                                    label="Descrição da Questão"
-                                    name="description"
-                                />
-                            </Box>
-                            <CustomTextField
-                                formik={formik}
-                                label="Descricão detalhada da Questão"
-                                name="detailedDescription"
-                                multiline
-                                rows={10}
-                            />
-                            <div className='flex mt-2'>
-                                <textarea id='code-editor'></textarea>
-                                <div className='w-[40%]'>
-                                    <div className='px-1 py-1 flex justify-end bg-[#1F2937] border border-gray-700'>
-                                        <Button variant='outlined' type='button'
-                                            onClick={handleRun}>Executar
-                                        </Button>
-                                    </div>
-                                    <div id="output"
-                                        className='bg-[#1F2937] border border-gray-700 h-[467.5px] overflow-y-auto p-6' />
-                                </div>
-                            </div>
-                            {formik.values.tests && formik.values.tests.map((test, index) => (
-                                <Box className='grid grid-cols-2 gap-x-4' key={`${test}-${index}`}>
-                                    <CustomTextField
-                                        formik={formik}
-                                        label={`Dados de entrada ${index + 1}`}
-                                        name={`tests.${index}.inputTest`}
-                                        value={test.inputTest}
-                                    />
-
-                                    <CustomTextField
-                                        formik={formik}
-                                        label={`Saída dos dados de entrada ${index + 1}`}
-                                        name={`tests.${index}.outputTest`}
-                                        value={test.outputTest}
-                                    />
-                                </Box>
-                            ))}
-                            <div className='flex justify-center mt-4 mb-6'>
-                                <Button sx={{ width: "50%" }} variant='outlined' type='button' onClick={addTest}>Adicionar mais testes</Button>
-                            </div>
-                        </Box>
-                        <Button
-                            fullWidth
-                            margin="normal"
-                            type='submit'
-                            color="primary"
-                            variant="contained"
-                        >
-                            Cadastrar Questão
-                        </Button>
-                    </form>
-                </Box>
-            </div>
-        </>
-    );
+                                  <CustomTextField
+                                      formik={formik}
+                                      label={`${index + 1}° saída dos dados`}
+                                      name={`tests.${index}.outputTest`}
+                                      value={test.outputTest}
+                                  />
+                              </Box>
+                          ))}
+                          <div className='flex justify-center mt-4 mb-6'>
+                              <Button
+                                sx={{ width: "50%" }}
+                                variant='outlined'
+                                type='button'
+                                onClick={addTest}
+                              >
+                                Adicionar caso de teste
+                              </Button>
+                          </div>
+                      </Box>
+                      <Button
+                          fullWidth
+                          margin="normal"
+                          type='submit'
+                          color="primary"
+                          variant="contained"
+                      >
+                        Cadastrar questão
+                      </Button>
+                  </form>
+              </Box>
+          </div>
+      </>
+  );
 };
 
 export default DatasQuestion
